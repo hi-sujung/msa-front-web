c8b4a482f9e776594525b920f10dbaf1d99dfb06
imsuhyen <ishyenn@gmail.com>
2024-06-13 00:51:46 +0900

    fix: 중복 코드 제거

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Register from './pages/Register';
-import Login from './pages/Login';
 import NoticeList from './pages/NoticeList';
 import ActivityList from './pages/ActivityList';
 import Activity from './pages/Activity';
