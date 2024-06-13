d4005648011f568eabeeed46d68734dbaf612dc6
imsuhyen <ishyenn@gmail.com>
2024-06-13 22:56:23 +0900

    feat: 홈 화면 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineFundProjectionScreen, AiOutlineNotification, AiOutlineRobot } from 'react-icons/ai';
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the Hi, Sujung Page</h1>
+    <div className="homeLinearGradient">
+      <div className="homeContainer">
+        <div className="homeOuterBox">
+          <h1 className="homeTitle">Hi, 수정이🔮</h1>
+          <div className="homeRow">
+            <Link to="/" className="homeButton homeFirstButton">
+              <AiOutlineFundProjectionScreen size={50} className="homeButtonIcon" />
+              <p className="homeButtonText">포트폴리오</p>
+            </Link>
+            <Link to="/" className="homeButton homeSecondButton">
+              <AiOutlineNotification size={50} className="homeButtonIcon" />
+              <p className="homeButtonText">공지사항 {'\n'} 조회</p>
+            </Link>
+            <Link to="/" className="homeButton homeThirdButton">
+              <AiOutlineNotification size={50} className="homeButtonIcon" />
+              <p className="homeButtonText homeThirdButtonText">대외활동 {'\n'} 조회</p>
+            </Link>
+          </div>
+        </div>
+        <Link to="/" className="homeChatBotButton">
+          <AiOutlineRobot size={20} />
+          <p className="homeChatBotButtonText">로그인</p>
+        </Link>
+      </div>
     </div>
   );
 }
